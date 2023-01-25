4c156d4179ebcf756b25e72715622da615b1d086
ArtemDubovitskiy <artemdubovicki@yandex.ru>
2023-01-26 00:42:36 +0300

    Добавлен модификатор доступа private для переменных и функций. Удалены лишние комментарии.

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -8,23 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // Связь Label из Storyboard с ViewController:
-    @IBOutlet weak var counterLabel: UILabel!
-    // Связь Button из Storyboard с ViewController:
-    @IBOutlet weak var counterAddButton: UIButton!
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var counterAddButton: UIButton!
     
-    private var count = 0 // Добавлена переменная с начальным значеним счетчика "0".
+    private var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        counterLabel.text = "Значение счетчика:\n \(count)" // Добавлено строковое свойство (с переносом строки) для отображения Label
+        counterLabel.text = "Значение счетчика:\n \(count)"
     }
-// Связь Action из Storyboard с ViewController (тап кнопки "Touch Up Inside"):
     @IBAction func tapCounterAddButton(_ sender: Any) {
-        count += 1 // увеличение значения счетчика на "1" после нажатия кнопки
-        counterLabel.text = "Значение счетчика:\n \(count)" // строковое свойство (с переносом строки) для отображения Label после нажатия кнопки
+        count += 1
+        counterLabel.text = "Значение счетчика:\n \(count)"
     }
     
 }
